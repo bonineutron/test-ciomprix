20ff1056ee280fe8f1fac3ee989ce12fc0797ab6
Andres Bonilla <mailofbonilla@gmail.com>
2022-04-27 17:41:03 -0500

    feat: add name property in search users

--- src/controllers/search-users.controller.js
+++ src/controllers/search-users.controller.js
@@ -3,5 +3,5 @@ import User from '../models/users';
 export const getUsers = async (req, res) => {
    // search for users excluding certain parameters
    const users = await User.find({}, { password: 0, _id: 0, createdAt: 0, updatedAt: 0 });
-   return res.json(users);
+   return res.json({ users });
 };
